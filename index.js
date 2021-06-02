793767bcf73510dbac47cfed6678406d417f2fe8
Jonathan Piland <jonathan.piland@duke.edu>
2021-06-02 10:40:19 -0400

    improve ui

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ let qIndex = 0; // X
 let qEnt = [];
 let qMem = [0, 0, 0, 1];
 
+let userTyping = false;
+
 function clickNum(num) {
     let input = document.getElementById(inputElementNames[qIndex]);
     input.value = num.toPrecision(3);
@@ -27,6 +29,8 @@ function clickNum(num) {
     qIndex++;
     qIndex %= 4;
     updateCursorPos();
+    userTyping = false;
+    document.activeElement.blur();
 }
 
 function clearInputs() {
@@ -67,15 +71,33 @@ function multiplyQuaternions() {
 function invert() {
     qMem = [-qMem[0], -qMem[1], -qMem[2], qMem[3]];
     updateOutput();
+    document.activeElement.blur();
 }
 
 function backspace() {
-    if (qIndex === 0 && document.getElementById(inputElementNames[3]).value == "") return;
-    qIndex--;
-    qIndex += 4;
-    qIndex %= 4;
+    if (!userTyping) {
+        if (qIndex === 0 && document.getElementById(inputElementNames[3]).value == "") {
+            if (document.getElementById(inputElementNames[2]).value == "") {
+                if (document.getElementById(inputElementNames[1]).value == "") {
+                    return;
+                } else {
+                    qIndex = 1;
+                }
+            } else {
+                qIndex = 2;
+            }
+        } else {
+            qIndex--;
+            qIndex += 4;
+            qIndex %= 4;
+        }
+    }
+    userTyping = false;
     document.getElementById(inputElementNames[qIndex]).value = "";
     qEnt[qIndex] = undefined;
+    while (qIndex > 0 && document.getElementById(inputElementNames[qIndex - 1]).value == "") {
+        qIndex--;
+    }
     updateCursorPos();
 }
 
@@ -86,19 +108,24 @@ function clearMem() {
 }
 
 function inputChange(inputId, event) {
-    let value = event.target.value;
-    qIndex = inputId;
-    qEnt[qIndex] = Number(value);
-    if (event.keyCode === 13) { // ENTER
-        qIndex++;
-        if (qIndex < 4) {
-            document.getElementById(inputElementNames[qIndex]).focus();
-        } else {
-            event.target.blur();
+    if (!isNaN(event.key) || event.keyCode === 13 || event.keyCode === 9) {
+        let value = event.target.value;
+        if (value != "") {
+            userTyping = true;
         }
-        qIndex %= 4;
+        qIndex = inputId;
+        qEnt[qIndex] = Number(value);
+        if (event.keyCode === 13) { // ENTER
+            qIndex++;
+            if (qIndex < 4) {
+                document.getElementById(inputElementNames[qIndex]).focus();
+            } else {
+                event.target.blur();
+            }
+            qIndex %= 4;
+        } 
+        updateCursorPos();
     }
-    updateCursorPos();
 }
 
 function multiply() {
@@ -107,8 +134,9 @@ function multiply() {
     }
     multiplyQuaternions();
     updateOutput();
-
+    clearInputs();
     lightRes();
+    document.activeElement.blur();
 }
 
 function updateCursorPos() {
@@ -120,6 +148,7 @@ function updateCursorPos() {
 function clickEntry(i) {
     qIndex = i;
     updateCursorPos();
+    userTyping = false;
 }
 
 function lightRes() {
@@ -134,4 +163,67 @@ function dimRes() {
     for (let i = 0; i < resDivs.length; i++) {
         resDivs[i].style.opacity = 0.5;
     }
+}
+
+function key(event) {
+    
+    let curr = document.getElementById(inputElementNames[qIndex]);
+    
+    if (!userTyping || document.getElementById(inputElementNames[qIndex]).value == "") {
+        switch(event.key) {
+            case 'u':
+                clickNum(1);
+                break;
+            case 'j':
+                clickNum(-1);
+                break;
+            case 'i':
+                clickNum(s);
+                break;
+            case 'k':
+                clickNum(-s);
+                break;
+            case 'o':
+                clickNum(h);
+                break;
+            case 'l':
+                clickNum(-h);
+                break;
+            case 'p':
+            case ';':
+                clickNum(0);
+                break;
+            case 'x':
+            case '*':
+                multiply();
+                break;
+            case 'c':
+                clearInputs();
+                break;
+            case 'v':
+                invert();
+                break;
+            case 'b':
+                backspace();
+                break;
+            case 'Backspace':
+                if(!userTyping) backspace();
+                else {
+                    curr.blur();
+                    userTyping = false;
+                    backspace();
+                }
+                break;
+            case 'n':
+            case 'm':
+                clearMem();
+                break;
+            default:
+                if (event.keyCode !== 13 && event.keyCode !== 9) {
+                    curr.focus();
+                    userTyping = true;
+                }
+        }
+    }
+
 }
